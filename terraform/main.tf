a4f32efe400a6a6ae2ba88a2b33a142b3b41bff5
palash das <palashpalash3149@gmail.com>
2025-05-16 18:58:25 +0530

    AMI CHANGED

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,6 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   count         = 3
-  ami           = "ami-0c1ac8a41498c1a9c" # Replace with a valid AMI in your region
+  ami           = "ami-084568db4383264d4" # Replace with a valid AMI in your region
   instance_type = "t3.micro"
 }
